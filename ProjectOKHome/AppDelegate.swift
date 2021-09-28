e4215fcf7afffef0d0f6bbef075e73a1ab913320
yudisthirogithub <yudis_deon@yahoo.com>
2021-09-28 11:36:50 +0700

    test

--- ProjectOKHome/AppDelegate.swift
+++ ProjectOKHome/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+//a
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
