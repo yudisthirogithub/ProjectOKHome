8e14ff7398510f532ae3d8ead73d70e96a2b37a2
yudisthirogithub <yudis_deon@yahoo.com>
2021-09-28 12:39:43 +0700

    This is first try commit to main

---
+++ ProjectOKHome/MainView/CollectionViewGit/RepoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RepoCollectionViewCell.swift
+//  ProjectOKHome
+//
+//  Created by Yudis Huang on 28/09/21.
+//
+
+import UIKit
+
+class RepoCollectionViewCell: UICollectionViewCell {
+    
+}
