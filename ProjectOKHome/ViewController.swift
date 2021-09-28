9c5ad1988fd4b1d5384e78f66ddf75ef3ce49966
yudisthirogithub <yudis_deon@yahoo.com>
2021-09-28 11:35:53 +0700

    Initial Commit

---
+++ ProjectOKHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectOKHome
+//
+//  Created by Yudis Huang on 28/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
