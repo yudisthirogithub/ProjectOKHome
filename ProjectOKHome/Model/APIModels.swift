21dd3aa0fdb7537ff568581279c31d96772898f2
yudisthirogithub <yudis_deon@yahoo.com>
2021-09-29 19:26:24 +0700

    Models for API

---
+++ ProjectOKHome/Model/APIModels.swift
@@ -0,0 +1,17 @@
+//
+//  APIModels.swift
+//  ProjectOKHome
+//
+//  Created by Yudis Huang on 29/09/21.
+//
+
+import Foundation
+
+struct Results : Codable {
+    
+    var full_name : String?
+    var description : String?
+    var stargazers_count : Int?
+    var forks_count : Int?
+    
+}
